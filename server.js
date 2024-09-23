553194acd9f08d6431ee29cbe60a4205fd01dd41
Vasyl Rusyn <90523681+VasyaR@users.noreply.github.com>
2024-09-23 18:48:24 +0300

    Update server.js for heroku PORT variable

--- server.js
+++ server.js
@@ -31,7 +31,11 @@ const mimeTypes = {
 
 // Hostname and Port
 const hostname = '0.0.0.0';  // Change this to 0.0.0.0 to allow external access
-const port = 3000;
+const PORT = process.env.PORT || 3000;  // Use the port Heroku gives us, or default to 3000 for local development
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 (async () => {
 const db = await open({
